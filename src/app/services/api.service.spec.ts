10ef884f4c3fe36fc467486bee2a8cff9efc26aa
haarsh1306 <hv41096@gmail.com>
2023-11-08 18:20:25 +0530

    serviec test

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,51 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
-  let service: ApiService;
+  let apiService: ApiService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ApiService],
+    });
+    apiService = TestBed.inject(ApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(apiService).toBeTruthy();
+  });
+
+  it('should fetch user data from the API', () => {
+    const mockUserData = { login: 'testuser' };
+    const username = 'testuser';
+
+    apiService.getUser(username).subscribe((data) => {
+      expect(data).toEqual(mockUserData);
+    });
+
+    const req = httpTestingController.expectOne(`https://api.github.com/users/${username}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUserData);
+  });
+
+  it('should fetch user repos data from the API', () => {
+    const mockReposData = [{ name: 'repo1' }, { name: 'repo2' }];
+    const username = 'testuser';
+
+    apiService.getUserRepos(username).subscribe((data) => {
+      expect(data).toEqual(mockReposData);
+    });
+
+    const req = httpTestingController.expectOne(`https://api.github.com/users/${username}/repos`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockReposData);
   });
 });
